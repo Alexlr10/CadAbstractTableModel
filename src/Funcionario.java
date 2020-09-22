7edb0e376a7d1a79453439c0c4c2cf7e62c1f16d
Alex lopes rocha <alex.rocha@ufvjm.edu.br>
2020-09-22 09:33:25 -0300

    commit inicial cadastrando funcionarios com absctractTableModel

---
+++ src/Funcionario.java
@@ -0,0 +1,46 @@
+
+/**
+ *
+ * @author SI04
+ */
+public class Funcionario {
+    private String Matricula;
+    private String Nome;
+    private String CPF;
+
+    public Funcionario(String Matricula, String Nome, String CPF) {
+        this.Matricula = Matricula;
+        this.Nome = Nome;
+        this.CPF = CPF;
+    }
+
+    public Funcionario() {
+    }
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String Matricula) {
+        this.Matricula = Matricula;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+    
+    
+    
+}
